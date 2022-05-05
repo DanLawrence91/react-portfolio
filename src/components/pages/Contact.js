5f03dfafd7eed30f6f8a92cfc1ee8c330f96d776
Daniel Lawrence <dan.lawrence0810@gmail.com>
2022-05-05 18:35:37 +0930

    started contact form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,7 +1,38 @@
-import React from "react";
+import { Flex, Box, Heading, Button, FormControl, FormLabel, FormErrorMessage, FormHelperText, Input, Textarea } from "@chakra-ui/react";
+import React, { useState } from "react";
 
 function Contact() {
-  return <div>Contact</div>;
+  // need to sort as fills out all boxes currently
+
+  return (
+    <Flex width="full" align="center" justifyContent={"center"}>
+      <Box p="2">
+        <Box textAlign={"center"}>
+          <Heading>Contact Me</Heading>
+        </Box>
+        <Box my={4} textAlign="left">
+          <form>
+            <FormControl>
+              <FormLabel>Name</FormLabel>
+              <Input id="name" type="text" placeholder="Enter name here" />
+            </FormControl>
+            <FormControl>
+              <FormLabel htmlFor="email">Email</FormLabel>
+              <Input id="email" type="email" placeholder="youremail@here.com" />
+            </FormControl>
+            <FormControl>
+              {/* Need to add error messages here */}
+              <FormLabel>Message</FormLabel>
+              <Textarea id="name" type="text" placeholder="Please leave your message here"></Textarea>
+            </FormControl>
+            <Button width="full" mt={4} type="submit">
+              Sign In
+            </Button>
+          </form>
+        </Box>
+      </Box>
+    </Flex>
+  );
 }
 
 export default Contact;
