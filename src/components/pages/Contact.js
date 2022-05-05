abc162a8aba18f925be4e057a6a0a796e05b39e7
Daniel Lawrence <dan.lawrence0810@gmail.com>
2022-05-06 09:27:24 +0930

    contact form added

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -5,28 +5,28 @@ function Contact() {
   // need to sort as fills out all boxes currently
 
   return (
-    <Flex width="full" align="center" justifyContent={"center"}>
-      <Box p="2" w={"50%"}>
+    <Flex width="full" align="center" justifyContent={"center"} p={5} mb={5}>
+      <Box p="8" w={"50%"} borderWidth={1} borderRadius={8} boxShadow="lg">
         <Box textAlign={"center"}>
           <Heading>Contact Me</Heading>
         </Box>
         <Box my={4} textAlign="left">
           <form>
-            <FormControl>
+            <FormControl isRequired p={2}>
               <FormLabel>Name</FormLabel>
               <Input id="name" type="text" placeholder="Enter name here" />
             </FormControl>
-            <FormControl>
+            <FormControl isRequired p={2}>
               <FormLabel htmlFor="email">Email</FormLabel>
               <Input id="email" type="email" placeholder="youremail@here.com" />
             </FormControl>
-            <FormControl>
+            <FormControl isRequired p={2}>
               {/* Need to add error messages here */}
               <FormLabel>Message</FormLabel>
               <Textarea id="name" type="text" placeholder="Please leave your message here"></Textarea>
             </FormControl>
-            <Button width="full" mt={4} type="submit">
-              Sign In
+            <Button width="full" mt={4} type="submit" backgroundColor={"cyan.400"} variant={"outline"}>
+              Contact
             </Button>
           </form>
         </Box>
