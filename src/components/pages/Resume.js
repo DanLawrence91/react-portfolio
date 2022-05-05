fb37f392192eaa87d0be7c4ccb14e5520a77e5ba
Daniel Lawrence <dan.lawrence0810@gmail.com>
2022-05-05 17:57:18 +0930

    outline of resume page added with downloadable cv

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,7 +1,33 @@
+import { Flex, Text, Link, UnorderedList, ListItem } from "@chakra-ui/react";
 import React from "react";
+import CV from "./assets/danLawrenceResume.pdf";
 
 function Resume() {
-  return <div>Resume</div>;
+  return (
+    <Flex direction={"column"}>
+      <Text>
+        Download my resume{" "}
+        <Link href={CV} isExternal>
+          here
+        </Link>
+      </Text>
+      <Text>Technical proficiencies include:</Text>
+      <UnorderedList>
+        <ListItem>HTML</ListItem>
+        <ListItem>CSS</ListItem>
+        <ListItem>JavaScript / TypeScript</ListItem>
+        <ListItem>Bootstrap / Tailwind CSS / Chakra UI</ListItem>
+        <ListItem>React</ListItem>
+        <ListItem>Node.js</ListItem>
+        <ListItem>REST APIs</ListItem>
+        <ListItem>MySQL</ListItem>
+        <ListItem>MongoDB / Mongoose</ListItem>
+        <ListItem>GraphQL</ListItem>
+        <ListItem>Express / Fastify</ListItem>
+        <ListItem>Sequelize / Prisma</ListItem>
+      </UnorderedList>
+    </Flex>
+  );
 }
 
 export default Resume;
