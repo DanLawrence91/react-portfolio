e5f07cb95343ab3fa0dc490f9ce92c63a05f4e8a
Daniel Lawrence <dan.lawrence0810@gmail.com>
2022-05-06 11:58:04 +0930

    navbar now highlights when active

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import { Flex, Heading, Text } from "@chakra-ui/react";
 import React from "react";
 import { NavLink } from "react-router-dom";
+import "./styles/nav.css";
 
 const MenuItems = (props) => {
   const { children, to } = props;
@@ -12,6 +13,25 @@ const MenuItems = (props) => {
 };
 
 function Header() {
+  const getClassName = () => {
+    switch (window.location.pathname) {
+      case "/react-portfolio":
+        return "active";
+
+      case "/portfolio":
+        return "active";
+
+      case "/contact":
+        return "active";
+
+      case "/resume":
+        return "active";
+
+      default:
+        return "inactive";
+    }
+  };
+
   return (
     <Flex as="nav" w="100vw" align="center" justify="space-between" wrap="wrap" mb={8} p={8} color="black" bg="yellow.100">
       <Flex align="center">
@@ -20,10 +40,18 @@ function Header() {
         </Heading>
       </Flex>
       <Flex align="center">
-        <MenuItems to="/react-portfolio">About Me</MenuItems>
-        <MenuItems to="/portfolio">Portfolio</MenuItems>
-        <MenuItems to="/contact">Contact</MenuItems>
-        <MenuItems to="/resume">Resume </MenuItems>
+        <MenuItems className={getClassName()} to="/react-portfolio">
+          About Me
+        </MenuItems>
+        <MenuItems className={getClassName()} to="/portfolio">
+          Portfolio
+        </MenuItems>
+        <MenuItems className={getClassName()} to="/contact">
+          Contact
+        </MenuItems>
+        <MenuItems className={getClassName()} to="/resume">
+          Resume{" "}
+        </MenuItems>
       </Flex>
     </Flex>
   );
