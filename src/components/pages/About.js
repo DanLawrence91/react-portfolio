240b0c99b02b36606ad7dd4c0bbc880c21e25e49
Daniel Lawrence <dan.lawrence0810@gmail.com>
2022-05-06 12:10:28 +0930

    changes to about me page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,8 +4,8 @@ import profPic from "./assets/pp.jpg";
 
 function About() {
   return (
-    <Flex p={10} m={5}>
-      <Image src={profPic} alt="headshot"></Image>
+    <Flex p={10} m={5} wrap={{ base: "wrap", lg: "nowrap" }} justifyContent={{ base: "center", lg: "flex-start" }}>
+      <Image src={profPic} alt="headshot" borderRadius={50}></Image>
       <Flex direction={"column"} p={10}>
         <Text my={7}>
           I am a full-stack web developer living in Adelaide, Australia having emigrated from the UK at the end of 2021! I am currently volunteering with an educational charity as a full stack developer whilst studying web development through the
